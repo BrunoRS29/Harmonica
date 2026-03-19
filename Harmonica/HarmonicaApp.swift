8350454e3a536dbacfe6ac7365f47b7362a29684
Bruno <bruno.rosantos@agi.com.br>
2026-03-19 09:15:18 -0300

    Initial Commit

---
+++ Harmonica/HarmonicaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HarmonicaApp.swift
+//  Harmonica
+//
+//  Created by Bruno Rodrigues dos Santos on 19/03/26.
+//
+
+import SwiftUI
+
+@main
+struct HarmonicaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
